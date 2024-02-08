30dff514c0111543ebc56f5f0942f8c8a3cc08ac
unknown <jylkychievdastan.0307@gmail.com>
2024-02-09 01:56:24 +0600

    Initialized layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { ToastContainer } from 'react-toastify'
 import MainRoutes from './router/MainRoutes'
+// import { LayoutComponent } from '@screens/layout/LayoutComponent'
+import LayoutComponent from '@screens/layout/LayoutComponent'
 import 'react-toastify/dist/ReactToastify.css'
 import './App.css'
 
 function App() {
   return (
     <>
-      <MainRoutes />
-      <ToastContainer />
+      {/* <MainRoutes />
+      <ToastContainer /> */}
+      <LayoutComponent />
     </>
   )
 }
---
+++ src/components/Sider/Sider.jsx
@@ -0,0 +1,8 @@
+import { Layout } from 'antd'
+import styles from './sider.module.scss'
+
+const Sider = () => {
+  return <Layout.Sider className={styles.sider}>Sider</Layout.Sider>
+}
+
+export default Sider
