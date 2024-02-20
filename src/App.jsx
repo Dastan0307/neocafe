3a471d2a0b63730c5b6267eaa7bfcb7e8abe6e63
SardanaMir <sardanadord@yandex.ru>
2024-02-20 15:15:30 +0300

    add sider

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,20 @@ import MainRoutes from './router/MainRoutes'
 // import { LayoutComponent } from '@screens/layout/LayoutComponent'
 import LayoutComponent from '@screens/layout/LayoutComponent'
 import 'react-toastify/dist/ReactToastify.css'
+import { Header } from 'antd/es/layout/layout'
+import Login from './screens/auth/login/Login'
+import Main from './screens/main/Main'
+
 import './App.css'
+import Sider from './components/Sider/Sider'
 
 function App() {
   return (
     <>
       {/* <MainRoutes />
       <ToastContainer /> */}
-      <LayoutComponent />
+      {/* <LayoutComponent /> */}
+      <Sider />
     </>
   )
 }
--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,8 @@
 import neocafeLogo from './images/neocafe.svg'
 import loginImg from './images/login-img.svg'
+import logo from './images/logo.svg'
+import orderIcon from './images/orderIcon.svg'
+import menuIcon from './images/menuIcon.svg'
+import profileIcon from './images/profileIcon.svg'
 
-export { neocafeLogo, loginImg }
+export { neocafeLogo, loginImg, logo, orderIcon, menuIcon, profileIcon }
